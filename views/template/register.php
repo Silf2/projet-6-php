cd61235cf6828e1397f0955adccdbf7b6d042f36
Silf2 <sylvain.guenet1@gmail.com>
2024-01-24 11:56:13 +0100

    Ajout de l'autoload et modification du nom des dossiers

---
+++ views/template/register.php
@@ -0,0 +1,18 @@
+<div class="registerPage">
+    <div>
+        <form action="index.php?action=registerUser" method="post">
+            <h1>Inscription</h1>
+            <p class="requiredValue">Pseudo</p>
+            <input type="text" name="username" id="username" required>
+            <p class="requiredValue">Adresse email</p>
+            <input type="email" name="email" id="email" required>
+            <p class="requiredValue">Mot de passe</p>
+            <input type="password" name="password" id="password" required>
+            </br>
+            <button>S'inscrire</button>
+        </form>
+        <p>Déjà inscrit ? Connectez-vous</p>
+    </div>
+
+    <img src="../images/marialaura-gionfriddo-50G3FvyQxX0-unsplash 1.png"/>
+</div>
